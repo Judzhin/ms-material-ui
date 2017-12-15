3486c74810127770f1164ec29abe781d55afd730
Judzhin Miles <info@msbios.com>
2017-12-15 22:00:50 +0200

    Fix

--- src/ButtonComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-
-const ButtonComponent = () => (
-    <RaisedButton label="Default" />
-);
-
-export default ButtonComponent;
