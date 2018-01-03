0e03c27c5f29301ac4ace27051c357fe630e67d9
Judzhin Miles <info@msbios.com>
2018-01-03 19:48:57 +0200

    Intermedaite commit

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+
+const Sidebar = () => (
+    <List>
+        <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+        <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+        <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+        <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+        <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+    </List>
+);
+
+export default Sidebar;
